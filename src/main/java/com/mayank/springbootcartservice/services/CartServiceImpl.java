703e19f8aea565a86dc5e814d5b01673da820c5f
Mayank <mayank1365@gmail.com>
2024-03-12 16:56:59 +0530

    Services

---
+++ src/main/java/com/mayank/springbootcartservice/services/CartServiceImpl.java
@@ -0,0 +1,132 @@
+package com.mayank.springbootcartservice.services;
+
+import com.mayank.springbootcartservice.dtos.CartRecieveDTO;
+import com.mayank.springbootcartservice.modals.Cart;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+
+
+import java.util.List;
+
+@Service
+public class CartServiceImpl implements  CartService{
+
+    private final String url = "https://fakestoreapi.com/carts";
+    private final RestTemplate restTemplate = new RestTemplate();
+    @Override
+    public List<Cart> getAllProducts() {
+
+        List<CartRecieveDTO> cartFetchDTO = restTemplate.exchange(
+                url,
+                HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<List<CartRecieveDTO>>() {}).getBody();
+
+        assert cartFetchDTO != null;
+
+        return cartFetchDTO.stream().map(this::mapToCart).toList();
+    }
+
+    private Cart mapToCart(CartRecieveDTO cartRecieveDTO) {
+
+        return new Cart(cartRecieveDTO.getId(), cartRecieveDTO.getUserId(), cartRecieveDTO.getDate(), cartRecieveDTO.getProducts());
+
+    }
+
+
+
+    @Override
+    public Cart getCart(long id) {
+
+        CartRecieveDTO cartRecieveDTO = restTemplate.getForObject(url + "/" + id, CartRecieveDTO.class);
+
+        assert cartRecieveDTO != null;
+
+        return mapToCart(cartRecieveDTO);
+    }
+
+    @Override
+    public List<Cart> limitedCarts(long limit) {
+        List<CartRecieveDTO> cartFetchDTO = restTemplate.exchange(
+                url + "?limit=" + limit,
+                HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<List<CartRecieveDTO>>() {}).getBody();
+
+        assert cartFetchDTO != null;
+
+        return cartFetchDTO.stream().map(this::mapToCart).toList();
+    }
+
+    @Override
+    public List<Cart> sortedCarts(String order) {
+
+        List<CartRecieveDTO> cartFetchDTO = restTemplate.exchange(
+                url + "?sort=" + order,
+                HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<List<CartRecieveDTO>>() {}).getBody();
+
+        assert cartFetchDTO != null;
+
+        return cartFetchDTO.stream().map(this::mapToCart).toList();
+
+    }
+
+    @Override
+    public List<Cart> inDateRange(String start, String end) {
+
+        List<CartRecieveDTO> cartFetchDTO = restTemplate.exchange(
+                url + "?startdate=" + start + "&enddate=" + end,
+                HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<List<CartRecieveDTO>>() {}).getBody();
+
+        assert cartFetchDTO != null;
+
+        return cartFetchDTO.stream().map(this::mapToCart).toList();
+
+    }
+
+    @Override
+    public List<Cart> userCart(long userId) {
+        List<CartRecieveDTO> cartFetchDTO = restTemplate.exchange(
+                url + "/user/" + userId,
+                HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<List<CartRecieveDTO>>() {}).getBody();
+
+        assert cartFetchDTO != null;
+
+        return cartFetchDTO.stream().map(this::mapToCart).toList();
+    }
+
+    @Override
+    public void addNewCart(Cart cart) {
+        CartRecieveDTO sendCart = mapToCardDTO(cart);
+        sendCart = restTemplate.postForObject(url, sendCart, CartRecieveDTO.class);
+
+//        The cart could be returned to the user as well but no need to do that right now
+
+    }
+
+    @Override
+    public void updateProduct(Cart cart) {
+        CartRecieveDTO sendCart = mapToCardDTO(cart);
+        restTemplate.put(url + "/" + sendCart.getId(), sendCart);
+    }
+
+    private CartRecieveDTO mapToCardDTO(Cart cart) {
+
+        return new CartRecieveDTO(cart.getId(), cart.getUserId(), cart.getDate(), cart.getProducts());
+
+    }
+
+    @Override
+    public void deleteCart(long id) {
+        restTemplate.delete(url + "/" + id);
+    }
+}
