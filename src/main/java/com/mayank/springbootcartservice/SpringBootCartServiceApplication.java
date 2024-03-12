260a751db7a1cf02d14100d15eae2fbff0db5020
Mayank <mayank1365@gmail.com>
2024-03-12 16:53:23 +0530

    initial commit

---
+++ src/main/java/com/mayank/springbootcartservice/SpringBootCartServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mayank.springbootcartservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCartServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCartServiceApplication.class, args);
+	}
+
+}
